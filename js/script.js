4568b8fcf23a0e4f8b6ce6ba0ca54d9cd16a7e96
bsia0831 <75350110+bsia0831@users.noreply.github.com>
2024-09-20 05:13:42 -0600

    base

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    navigationTooltips: ['First Section', 'Second Section', 'Third Section'],
+    navigation: true,
+    slidesNavigation: true,
+    controlArrows: false,
+    licenseKey: 'xxxxxxxxxxxxxxxxxxxxxxxxx' // 여기에 라이센스 키를 입력하세요
+});
